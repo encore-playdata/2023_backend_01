2ee3b69224e16a3725560eebe2d5e50fac794be4
tjdanqkr <tjdanqkr@gmail.com>
2023-05-26 17:45:21 +0900

    homework

--- java/Day5/src/Example4.java
+++ java/Day5/src/Example4.java
@@ -14,5 +14,11 @@ public class Example4 {
 //		{number:2,name:김, age:30},
 //		{number:3,name:이, age:40},
 //		{number:4,name:권, age:null}]
+		
+		String excel2 = "수\t상호명\t좌표\t지역\n" + 
+				"1\t피자\t37.01,127.01\t대구\n" + 
+				"2\t치킨\t37.03,127.02\t대전\n" + 
+				"3\t햄버거\t37.13,127.02\t서울\n" + 
+				"4\t감자탕\t36.13,127.02\t부산";
 	}
 }
