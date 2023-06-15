2f942efdcc1b95221e985857f66120b6c2f4ba35
tjdanqkr <tjdanqkr@gmail.com>
2023-06-15 10:27:02 +0900

    user servlet

---
+++ servlet/todos/src/main/java/com/playdata/todos/config/JdbcConnection.java
@@ -0,0 +1,28 @@
+package com.playdata.todos.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+    private final String url = "jdbc:mysql://localhost:3306/todos" +
+            "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
+    private final String username = "root";
+    private final String password = "1q2w3e4r!!";
+
+    public Connection getJdbc()  {
+        Connection conn;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager
+                    .getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        System.out.println("연동 성공");
+        return conn;
+    }
+}
